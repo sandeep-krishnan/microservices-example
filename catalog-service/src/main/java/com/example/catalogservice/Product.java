fd0540a32fe9f249087f29c742969dbc4e844001
Sandeep Krishnan <www.sandeepkrishnan@gmail.com>
2019-03-04 08:40:17 +0530

    adding test config & creating schemas during startup

---
+++ catalog-service/src/main/java/com/example/catalogservice/Product.java
@@ -0,0 +1,20 @@
+package com.example.catalogservice;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+public class Product {
+    @Id
+    @SequenceGenerator(name="product_pkey_sequence",sequenceName="product_pkey_sequence", initialValue=0, allocationSize=1)
+    @GeneratedValue(strategy= GenerationType.SEQUENCE, generator="product_pkey_sequence")
+    private Long id;
+
+    private String name;
+
+    private String description;
+}
--- init/init.sql
+++ init/init.sql
@@ -1,1 +1,7 @@
-create table sometable3(id int);
+create table product(
+  id bigint PRIMARY KEY,
+  name varchar(200),
+  description varchar(1024)
+);
+
+CREATE SEQUENCE product_pkey_sequence START 1;
