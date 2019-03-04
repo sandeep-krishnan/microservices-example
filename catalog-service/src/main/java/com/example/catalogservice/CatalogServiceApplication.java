386c87b224996e46a6125e1326bc9db22b1c7275
Sandeep Krishnan <www.sandeepkrishnan@gmail.com>
2019-03-04 08:39:01 +0530

    inserting products during startup

--- catalog-service/src/main/java/com/example/catalogservice/CatalogServiceApplication.java
+++ catalog-service/src/main/java/com/example/catalogservice/CatalogServiceApplication.java
@@ -1,13 +1,25 @@
 package com.example.catalogservice;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CatalogServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CatalogServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CatalogServiceApplication.class, args);
+    }
 
+
+    @Bean
+    public CommandLineRunner demoData(ProductRepository repository) {
+        return args -> {
+            Product product = new Product();
+            product.setName("Rice");
+            product.setDescription("Lots of rice");
+            repository.save(product);
+        };
+    }
 }
